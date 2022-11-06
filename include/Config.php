3c29ac0cc9a46b2a7db1a403acff1ea9d40571b9
drsaoudimo <112994958+drsaoudimo@users.noreply.github.com>
2022-11-06 17:45:05 +0100

    Update Config.php

--- include/Config.php
+++ include/Config.php
@@ -1,7 +1,7 @@
 <?php
-$databaseHost = 'localhost';
-$databaseUsername = '';
-$databasePassword = '';
-$databaseName = '';
+$databaseHost = 'ec2-3-227-68-43.compute-1.amazonaws.com';
+$databaseUsername = 'vdvqukkqojpxcx';
+$databasePassword = 'ca0db8f4a49d007703d8c2923e5b7ad7c33f5c564e26e959269ee8f0be3aa219';
+$databaseName = 'd4q20qm01628s';
 $path2Phps = '';
 $baseurl = '';?>

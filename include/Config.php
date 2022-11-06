3a061e098510e30c8dd89a06a53e12ed88ab24ea
drsaoudimo <112994958+drsaoudimo@users.noreply.github.com>
2022-11-06 22:23:52 +0100

    Update Config.php

--- include/Config.php
+++ include/Config.php
@@ -3,5 +3,5 @@ $databaseHost = 'ec2-44-205-177-160.compute-1.amazonaws.com';
 $databaseUsername = 'kxnejnuswwwqot';
 $databasePassword = '346cbdb84eb2bcb934788e7e427b0452660f3eb09a88bf28ebc173dd9f9721df';
 $databaseName = 'dcu7aq99o0njc4';
-$path2Phps = '';
-$baseurl = '';?>
+$path2Phps = 'https://socialmedical.herokuapp.com/';
+$baseurl = 'https://socialmedical.herokuapp.com/';?>
